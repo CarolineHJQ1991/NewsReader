14311661816588361480fadce6621d9375b2286a
caroline <caroline.hjq@gmail.com>
2015-12-08 22:05:00 +0800

    Build project and add some groups to manage code better

---
+++ NewsReader/Define/Define.h
@@ -0,0 +1,13 @@
+//
+//  Define.h
+//  NewsReader
+//
+//  Created by Caroline.H on 12/8/15.
+//  Copyright © 2015 Caroline.H. All rights reserved.
+//
+
+#ifndef Define_h
+#define Define_h
+
+
+#endif /* Define_h */
